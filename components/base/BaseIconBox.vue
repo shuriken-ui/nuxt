85bf7f896c7701d2d969c01a0f2767815053731f
Biya Paul <77636493+bpsmartdesign@users.noreply.github.com>
2023-06-15 15:26:35 +0100

    refactor: review class definition for forms components (part 2) (#19)

--- components/base/BaseIconBox.vue
+++ components/base/BaseIconBox.vue
@@ -47,95 +47,110 @@ const appConfig = useAppConfig()
 const shape = computed(
   () => props.shape ?? appConfig.nui.defaultShapes?.iconBox
 )
+
+const solidColorVariant = {
+  info: 'bg-info-500 text-white',
+  success: 'bg-success-500 text-white',
+  warning: 'bg-warning-500 text-white',
+  danger: 'bg-danger-500 text-white',
+  primary: 'bg-primary-500 text-white',
+  light: 'text-muted-800 bg-white',
+}
+const pastelColorVariant = {
+  info: 'bg-info-500/10 text-info-500',
+  success: 'bg-success-500/10 text-success-500',
+  warning: 'bg-warning-500/10 text-primary-500',
+  danger: 'bg-danger-500/10 text-danger-500',
+  primary: 'bg-primary-500/10 text-primary-500',
+  light: 'text-muted-800 bg-white/10',
+}
+const outlineColorVariant = {
+  info: 'border-2 border-current text-info-500',
+  success: 'border-2 border-current text-success-500',
+  warning: 'border-2 border-current text-warning-500',
+  danger: 'border-2 border-current text-danger-500',
+  primary: 'border-2 border-current text-primary-500',
+  light: 'border-2 border-current text-muted-800',
+}
+
+const sizeStyle = {
+  xs: 'h-8 w-8',
+  sm: 'h-10 w-10',
+  md: 'h-12 w-12',
+  lg: 'h-16 w-16',
+  xl: 'h-20 w-20',
+  '2xl': 'h-24 w-24',
+}
+
+const curvedStyle = {
+  xs: 'rounded-xl',
+  sm: 'rounded-xl',
+  md: 'rounded-2xl',
+  lg: 'rounded-2xl',
+  xl: 'rounded-3xl',
+  '2xl': 'rounded-3xl',
+}
+const roundedStyle = {
+  xs: 'rounded-lg',
+  sm: 'rounded-lg',
+  md: 'rounded-xl',
+  lg: 'rounded-xl',
+  xl: 'rounded-2xl',
+  '2xl': 'rounded-2xl',
+}
+
+const colorStyle = computed(() => {
+  let colors: any =
+    props.flavor === 'solid'
+      ? solidColorVariant
+      : props.flavor === 'outline'
+      ? outlineColorVariant
+      : pastelColorVariant
+
+  colors.default =
+    'bg-muted-100 dark:bg-muted-700 text-muted-600 dark:text-muted-200'
+
+  return colors
+})
+const shapeStyle = computed(() =>
+  shape.value === 'full'
+    ? 'rounded-full'
+    : shape.value === 'rounded'
+    ? roundedStyle[props.size]
+    : curvedStyle[props.size]
+)
+
+const classes = computed(() => {
+  let ret = [
+    'relative inline-flex shrink-0 items-center justify-center',
+    sizeStyle[props.size],
+    props.color ? colorStyle.value[props.color] : '',
+    shapeStyle.value,
+  ]
+  props.bordered &&
+    props.flavor === 'pastel' &&
+    ret.push('border-2 border-current')
+  props.mask !== undefined && props.shape === 'straight' && ret.push('nui-mask')
+  props.mask === 'hex' && props.shape === 'straight' && ret.push('nui-mask-hex')
+  props.mask === 'hexed' &&
+    props.shape === 'straight' &&
+    ret.push('nui-mask-hexed')
+  props.mask === 'deca' &&
+    props.shape === 'straight' &&
+    ret.push('nui-mask-deca')
+  props.mask === 'blob' &&
+    props.shape === 'straight' &&
+    ret.push('nui-mask-blob')
+  props.mask === 'diamond' &&
+    props.shape === 'straight' &&
+    ret.push('nui-mask-diamond')
+
+  return ret
+})
 </script>
 
 <template>
-  <div
-    class="relative inline-flex shrink-0 items-center justify-center"
-    :class="[
-      props.size === 'xs' && 'h-8 w-8',
-      props.size === 'sm' && 'h-10 w-10',
-      props.size === 'md' && 'h-12 w-12',
-      props.size === 'lg' && 'h-16 w-16',
-      props.size === 'xl' && 'h-20 w-20',
-      props.size === '2xl' && 'h-24 w-24',
-      shape === 'full' && 'rounded-full',
-      shape === 'rounded' && props.size === 'xs' && 'rounded-lg',
-      shape === 'rounded' && props.size === 'sm' && 'rounded-lg',
-      shape === 'rounded' && props.size === 'md' && 'rounded-xl',
-      shape === 'rounded' && props.size === 'lg' && 'rounded-xl',
-      shape === 'rounded' && props.size === 'xl' && 'rounded-2xl',
-      shape === 'rounded' && props.size === '2xl' && 'rounded-2xl',
-      shape === 'curved' && props.size === 'xs' && 'rounded-xl',
-      shape === 'curved' && props.size === 'sm' && 'rounded-xl',
-      shape === 'curved' && props.size === 'md' && 'rounded-2xl',
-      shape === 'curved' && props.size === 'lg' && 'rounded-2xl',
-      shape === 'curved' && props.size === 'xl' && 'rounded-3xl',
-      shape === 'curved' && props.size === '2xl' && 'rounded-3xl',
-      props.bordered && props.flavor === 'pastel' && 'border-2 border-current',
-      props.color === 'default' &&
-        'bg-muted-100 dark:bg-muted-700 text-muted-600 dark:text-muted-200',
-      props.color === 'primary' &&
-        props.flavor === 'pastel' &&
-        'bg-primary-500/10 text-primary-500',
-      props.color === 'info' &&
-        props.flavor === 'pastel' &&
-        'bg-info-500/10 text-info-500',
-      props.color === 'success' &&
-        props.flavor === 'pastel' &&
-        'bg-success-500/10 text-success-500',
-      props.color === 'warning' &&
-        props.flavor === 'pastel' &&
-        'bg-warning-500/10 text-warning-500',
-      props.color === 'danger' &&
-        props.flavor === 'pastel' &&
-        'bg-danger-500/10 text-danger-500',
-      props.color === 'light' &&
-        props.flavor === 'pastel' &&
-        'text-muted-800 bg-white/10',
-      props.color === 'primary' &&
-        props.flavor === 'solid' &&
-        'bg-primary-500 text-white',
-      props.color === 'info' &&
-        props.flavor === 'solid' &&
-        'bg-info-500 text-white',
-      props.color === 'success' &&
-        props.flavor === 'solid' &&
-        'bg-success-500 text-white',
-      props.color === 'warning' &&
-        props.flavor === 'solid' &&
-        'bg-warning-500 text-white',
-      props.color === 'danger' &&
-        props.flavor === 'solid' &&
-        'bg-danger-500 text-white',
-      props.color === 'light' &&
-        props.flavor === 'solid' &&
-        'text-muted-800 bg-white',
-      props.flavor === 'outline' && 'border-2 border-current',
-      props.color === 'primary' &&
-        props.flavor === 'outline' &&
-        'text-primary-500',
-      props.color === 'info' && props.flavor === 'outline' && 'text-info-500',
-      props.color === 'success' &&
-        props.flavor === 'outline' &&
-        'text-success-500',
-      props.color === 'warning' &&
-        props.flavor === 'outline' &&
-        'text-warning-500',
-      props.color === 'danger' &&
-        props.flavor === 'outline' &&
-        'text-danger-500',
-      props.color === 'light' && props.flavor === 'outline' && 'text-muted-800',
-      props.mask !== undefined && props.shape === 'straight' && 'nui-mask',
-      props.mask === 'hex' && props.shape === 'straight' && 'nui-mask-hex',
-      props.mask === 'hexed' && props.shape === 'straight' && 'nui-mask-hexed',
-      props.mask === 'deca' && props.shape === 'straight' && 'nui-mask-deca',
-      props.mask === 'blob' && props.shape === 'straight' && 'nui-mask-blob',
-      props.mask === 'diamond' &&
-        props.shape === 'straight' &&
-        'nui-mask-diamond',
-    ]"
-  >
+  <div :class="classes">
     <slot></slot>
   </div>
 </template>
