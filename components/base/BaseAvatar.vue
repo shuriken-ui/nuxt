fd3c69411d7d0c457b74d826be4ad5fb9ba61005
Biya Paul <77636493+bpsmartdesign@users.noreply.github.com>
2023-06-15 15:22:22 +0100

    refactor: review class definition for base components (#17)

--- components/base/BaseAvatar.vue
+++ components/base/BaseAvatar.vue
@@ -62,40 +62,141 @@ const props = withDefaults(
 )
 const appConfig = useAppConfig()
 const shape = computed(() => props.shape ?? appConfig.nui.defaultShapes?.avatar)
+
+const curvedStyle = {
+  xxs: 'rounded-md',
+  xs: 'rounded-lg',
+  sm: 'rounded-xl',
+  md: 'rounded-2xl',
+  lg: 'rounded-2xl',
+  xl: 'rounded-3xl',
+  '2xl': 'rounded-3xl',
+  '3xl': 'rounded-3xl',
+  '4xl': 'rounded-3xl',
+}
+
+const avatarSizeStyle = {
+  xxs: 'h-6 w-6 text-xs',
+  xs: 'h-8 w-8 text-sm',
+  sm: 'h-10 w-10 text-sm',
+  md: 'h-12 w-12 text-sm',
+  lg: 'h-16 w-16 text-sm',
+  xl: 'h-20 w-20 text-base',
+  '2xl': 'h-24 w-24 text-xl',
+  '3xl': 'h-28 w-28 text-2xl',
+  '4xl': 'h-32 w-32 text-3xl',
+}
+const avatarRoundStyle = {
+  xxs: 'rounded',
+  xs: 'rounded',
+  sm: 'rounded-md',
+  md: 'rounded-lg',
+  lg: 'rounded-lg',
+  xl: 'rounded-xl',
+  '2xl': 'rounded-xl',
+  '3xl': 'rounded-xl',
+  '4xl': 'rounded-xl',
+}
+
+const badgeSizeStyle = {
+  xxs: 'h-3 w-3',
+  xs: 'h-4 w-4',
+  sm: 'h-5 w-5',
+  md: 'h-5 w-5',
+  lg: 'h-6 w-6',
+  xl: 'h-8 w-8',
+  '2xl': 'h-10 w-10',
+  '3xl': 'h-10 w-10',
+  '4xl': 'h-12 w-12',
+}
+const badgeRoundStyle = {
+  xxs: 'rounded',
+  xs: 'rounded',
+  sm: 'rounded-lg',
+  md: 'rounded-xl',
+  lg: 'rounded-xl',
+  xl: 'rounded-2xl',
+  '2xl': 'rounded-2xl',
+  '3xl': 'rounded-2xl',
+  '4xl': 'rounded-2xl',
+}
+
+const dotSizeStyle = {
+  xxs: 'h-1.5 w-1.5',
+  xs: 'h-2 w-2',
+  sm: 'h-2 w-2',
+  md: 'h-3 w-3',
+  lg: 'h-3 w-3',
+  xl: 'h-4 w-4',
+  '2xl': 'h-4 w-4',
+  '3xl': 'h-4 w-4',
+  '4xl': 'h-4 w-4',
+}
+const dotTypeStyle = {
+  success: 'bg-success-500',
+  primary: 'bg-primary-500',
+  info: 'bg-info-500',
+  warning: 'bg-warning-500',
+  danger: 'bg-danger-500',
+  pink: 'bg-pink-500',
+  yellow: 'bg-yellow-500',
+}
+
+const badgePosStyle = computed(() => {
+  let ret = ''
+
+  if (props.size === '4xl') ret = '-bottom-2 -end-2'
+  if (['2xl', '3xl'].includes(props.size)) ret = '-bottom-1.5 -end-1.5'
+  if (['xs', 'sm', 'md', 'lg', 'xl'].includes(props.size))
+    ret = '-bottom-1 -end-1'
+
+  return props.shape === 'full' ? '' : ret
+})
+const dotPosStyle = computed(() => {
+  let ret = ''
+  const dotFullPosStyle = {
+    xxs: '',
+    xs: 'end-0 top-0',
+    sm: 'end-0.5 top-0.5',
+    md: 'end-0.5 top-0.5',
+    lg: 'end-1 top-1',
+    xl: 'end-1 top-1',
+    '2xl': 'end-1.5 top-1.5',
+    '3xl': 'end-2 top-2',
+    '4xl': 'end-2.5 top-2.5',
+  }
+
+  if (props.shape === 'curved') {
+    ret = ['xs', 'lg'].includes(props.size)
+      ? '-end-0.5 -top-0.5'
+      : '-end-0 -top-0'
+  } else {
+    ret = props.shape === 'full' ? dotFullPosStyle[props.size] : 'end-0 top-0'
+  }
+
+  return ret
+})
+
+const avatarShapeStyle = {
+  full: 'rounded-full',
+  rounded: avatarRoundStyle[props.size],
+  curved: curvedStyle[props.size],
+  straight: '',
+}
+const badgeShapeStyle = {
+  full: 'rounded-full',
+  rounded: badgeRoundStyle[props.size],
+  curved: curvedStyle[props.size],
+  straight: '',
+}
 </script>
 
 <template>
   <div
     class="relative inline-flex shrink-0 items-center justify-center outline-none"
     :class="[
-      props.size === 'xxs' && `h-6 w-6`,
-      props.size === 'xs' && `h-8 w-8`,
-      props.size === 'sm' && `h-10 w-10`,
-      props.size === 'md' && `h-12 w-12`,
-      props.size === 'lg' && `h-16 w-16`,
-      props.size === 'xl' && `h-20 w-20`,
-      props.size === '2xl' && `h-24 w-24`,
-      props.size === '3xl' && `h-28 w-28`,
-      props.size === '4xl' && `h-32 w-32`,
-      shape === 'full' && 'rounded-full',
-      shape === 'rounded' && props.size === 'xxs' && 'rounded',
-      shape === 'rounded' && props.size === 'xs' && 'rounded',
-      shape === 'rounded' && props.size === 'sm' && 'rounded-md',
-      shape === 'rounded' && props.size === 'md' && 'rounded-lg',
-      shape === 'rounded' && props.size === 'lg' && 'rounded-lg',
-      shape === 'rounded' && props.size === 'xl' && 'rounded-xl',
-      shape === 'rounded' && props.size === '2xl' && 'rounded-xl',
-      shape === 'rounded' && props.size === '3xl' && 'rounded-xl',
-      shape === 'rounded' && props.size === '4xl' && 'rounded-xl',
-      shape === 'curved' && props.size === 'xxs' && 'rounded-md',
-      shape === 'curved' && props.size === 'xs' && 'rounded-lg',
-      shape === 'curved' && props.size === 'sm' && 'rounded-xl',
-      shape === 'curved' && props.size === 'md' && 'rounded-2xl',
-      shape === 'curved' && props.size === 'lg' && 'rounded-2xl',
-      shape === 'curved' && props.size === 'xl' && 'rounded-3xl',
-      shape === 'curved' && props.size === '2xl' && 'rounded-3xl',
-      shape === 'curved' && props.size === '3xl' && 'rounded-3xl',
-      shape === 'curved' && props.size === '4xl' && 'rounded-3xl',
+      avatarSizeStyle[props.size],
+      avatarShapeStyle[shape],
       props.mask !== undefined && props.shape === 'straight' && 'nui-mask',
       props.mask === 'hex' && props.shape === 'straight' && 'nui-mask-hex',
       props.mask === 'hexed' && props.shape === 'straight' && 'nui-mask-hexed',
@@ -108,28 +209,7 @@ const shape = computed(() => props.shape ?? appConfig.nui.defaultShapes?.avatar)
   >
     <div
       class="flex h-full w-full items-center justify-center overflow-hidden text-center transition-all duration-300"
-      :class="[
-        shape === 'full' && 'rounded-full',
-        shape === 'rounded' && props.size === 'xxs' && 'rounded',
-        shape === 'rounded' && props.size === 'xs' && 'rounded',
-        shape === 'rounded' && props.size === 'sm' && 'rounded-lg',
-        shape === 'rounded' && props.size === 'md' && 'rounded-xl',
-        shape === 'rounded' && props.size === 'lg' && 'rounded-xl',
-        shape === 'rounded' && props.size === 'xl' && 'rounded-2xl',
-        shape === 'rounded' && props.size === '2xl' && 'rounded-2xl',
-        shape === 'rounded' && props.size === '3xl' && 'rounded-2xl',
-        shape === 'rounded' && props.size === '4xl' && 'rounded-2xl',
-
-        shape === 'curved' && props.size === 'xxs' && 'rounded-md',
-        shape === 'curved' && props.size === 'xs' && 'rounded-lg',
-        shape === 'curved' && props.size === 'sm' && 'rounded-xl',
-        shape === 'curved' && props.size === 'md' && 'rounded-2xl',
-        shape === 'curved' && props.size === 'lg' && 'rounded-2xl',
-        shape === 'curved' && props.size === 'xl' && 'rounded-3xl',
-        shape === 'curved' && props.size === '2xl' && 'rounded-3xl',
-        shape === 'curved' && props.size === '3xl' && 'rounded-3xl',
-        shape === 'curved' && props.size === '4xl' && 'rounded-3xl',
-      ]"
+      :class="badgeShapeStyle[shape]"
     >
       <slot>
         <img
@@ -138,15 +218,7 @@ const shape = computed(() => props.shape ?? appConfig.nui.defaultShapes?.avatar)
           class="max-h-full max-w-full object-cover shadow-sm dark:border-transparent"
           :class="[
             props.srcDark ? 'dark:hidden' : '',
-            props.size === 'xxs' && `h-6 w-6`,
-            props.size === 'xs' && `h-8 w-8`,
-            props.size === 'sm' && `h-10 w-10`,
-            props.size === 'md' && `h-12 w-12`,
-            props.size === 'lg' && `h-16 w-16`,
-            props.size === 'xl' && `h-20 w-20`,
-            props.size === '2xl' && `h-24 w-24`,
-            props.size === '3xl' && `h-28 w-28`,
-            props.size === '4xl' && `h-32 w-32`,
+            avatarSizeStyle[props.size],
           ]"
         />
 
@@ -154,33 +226,13 @@ const shape = computed(() => props.shape ?? appConfig.nui.defaultShapes?.avatar)
           v-if="props.src && props.srcDark"
           :src="props.srcDark"
           class="hidden max-w-full object-cover shadow-sm dark:block dark:border-transparent"
-          :class="[
-            props.size === 'xxs' && `h-6 w-6`,
-            props.size === 'xs' && `h-8 w-8`,
-            props.size === 'sm' && `h-10 w-10`,
-            props.size === 'md' && `h-12 w-12`,
-            props.size === 'lg' && `h-16 w-16`,
-            props.size === 'xl' && `h-20 w-20`,
-            props.size === '2xl' && `h-24 w-24`,
-            props.size === '3xl' && `h-28 w-28`,
-            props.size === '4xl' && `h-32 w-32`,
-          ]"
+          :class="[avatarSizeStyle[props.size]]"
         />
 
         <span
           v-if="!props.src"
           class="font-heading text-center font-medium uppercase"
-          :class="[
-            props.size === 'xxs' && `text-xs`,
-            props.size === 'xs' && `text-sm`,
-            props.size === 'sm' && `text-sm`,
-            props.size === 'md' && `text-sm`,
-            props.size === 'lg' && `text-base`,
-            props.size === 'xl' && `text-lg`,
-            props.size === '2xl' && `text-xl`,
-            props.size === '3xl' && `text-2xl`,
-            props.size === '4xl' && `text-3xl`,
-          ]"
+          :class="[avatarSizeStyle[props.size]]"
         >
           {{ props.text }}
         </span>
@@ -190,42 +242,7 @@ const shape = computed(() => props.shape ?? appConfig.nui.defaultShapes?.avatar)
     <div
       v-if="'badge' in $slots || props.badgeSrc"
       class="dark:bg-muted-800 absolute z-10 block overflow-hidden rounded-full bg-white"
-      :class="[
-        props.size === 'xxs' && `h-3 w-3`,
-        props.size === 'xs' && `h-4 w-4`,
-        props.size === 'sm' && `h-5 w-5`,
-        props.size === 'md' && `h-5 w-5`,
-        props.size === 'lg' && `h-6 w-6`,
-        props.size === 'xl' && `h-8 w-8`,
-        props.size === '2xl' && `h-10 w-10`,
-        props.size === '3xl' && `h-10 w-10`,
-        props.size === '4xl' && `h-12 w-12`,
-        shape === 'full' && `bottom-0 end-0`,
-        shape === 'straight' && props.size === 'xs' && `-bottom-1 -end-1`,
-        shape === 'straight' && props.size === 'sm' && `-bottom-1 -end-1`,
-        shape === 'straight' && props.size === 'md' && `-bottom-1 -end-1`,
-        shape === 'straight' && props.size === 'lg' && `-bottom-1 -end-1`,
-        shape === 'straight' && props.size === 'xl' && `-bottom-1 -end-1`,
-        shape === 'straight' && props.size === '2xl' && `-bottom-1.5 -end-1.5`,
-        shape === 'straight' && props.size === '3xl' && `-bottom-1.5 -end-1.5`,
-        shape === 'straight' && props.size === '4xl' && `-bottom-2 -end-2`,
-        shape === 'rounded' && props.size === 'xs' && `-bottom-1 -end-1`,
-        shape === 'rounded' && props.size === 'sm' && `-bottom-1 -end-1`,
-        shape === 'rounded' && props.size === 'md' && `-bottom-1 -end-1`,
-        shape === 'rounded' && props.size === 'lg' && `-bottom-1 -end-1`,
-        shape === 'rounded' && props.size === 'xl' && `-bottom-1 -end-1`,
-        shape === 'rounded' && props.size === '2xl' && `-bottom-1.5 -end-1.5`,
-        shape === 'rounded' && props.size === '3xl' && `-bottom-1.5 -end-1.5`,
-        shape === 'rounded' && props.size === '4xl' && `-bottom-2 -end-2`,
-        shape === 'curved' && props.size === 'xs' && `-bottom-1 -end-1`,
-        shape === 'curved' && props.size === 'sm' && `-bottom-1 -end-1`,
-        shape === 'curved' && props.size === 'md' && `-bottom-1 -end-1`,
-        shape === 'curved' && props.size === 'lg' && `-bottom-1 -end-1`,
-        shape === 'curved' && props.size === 'xl' && `-bottom-1 -end-1`,
-        shape === 'curved' && props.size === '2xl' && `-bottom-1.5 -end-1.5`,
-        shape === 'curved' && props.size === '3xl' && `-bottom-1.5 -end-1.5`,
-        shape === 'curved' && props.size === '4xl' && `-bottom-2 -end-2`,
-      ]"
+      :class="[badgeSizeStyle[props.size], badgePosStyle]"
     >
       <slot name="badge">
         <img
@@ -240,42 +257,7 @@ const shape = computed(() => props.shape ?? appConfig.nui.defaultShapes?.avatar)
     <span
       v-if="props.dot"
       class="dark:border-muted-800 absolute block rounded-full border border-white"
-      :class="[
-        props.size === 'xxs' && `h-1.5 w-1.5`,
-        props.size === 'xs' && `h-2 w-2`,
-        props.size === 'sm' && `h-2 w-2`,
-        props.size === 'md' && `h-3 w-3`,
-        props.size === 'lg' && `h-3 w-3`,
-        props.size === 'xl' && `h-4 w-4`,
-        props.size === '2xl' && `h-4 w-4`,
-        props.size === '3xl' && `h-4 w-4`,
-        props.size === '4xl' && `h-4 w-4`,
-        props.shape === 'straight' && 'end-0 top-0',
-        props.shape === 'rounded' && 'end-0 top-0',
-        props.shape === 'full' && props.size === 'xs' && `end-0 top-0`,
-        props.shape === 'full' && props.size === 'sm' && `end-0.5 top-0.5`,
-        props.shape === 'full' && props.size === 'md' && `end-0.5 top-0.5`,
-        props.shape === 'full' && props.size === 'lg' && `end-1 top-1`,
-        props.shape === 'full' && props.size === 'xl' && `end-1 top-1`,
-        props.shape === 'full' && props.size === '2xl' && `end-1.5 top-1.5`,
-        props.shape === 'full' && props.size === '3xl' && `end-2 top-2`,
-        props.shape === 'full' && props.size === '4xl' && `end-2.5 top-2.5`,
-        props.shape === 'curved' && props.size === 'xs' && `-end-0.5 -top-0.5`,
-        props.shape === 'curved' && props.size === 'sm' && `end-0 top-0`,
-        props.shape === 'curved' && props.size === 'md' && `end-0 top-0`,
-        props.shape === 'curved' && props.size === 'lg' && `-end-0.5 -top-0.5 `,
-        props.shape === 'curved' && props.size === 'xl' && `-end-0 -top-0`,
-        props.shape === 'curved' && props.size === '2xl' && `-end-0 -top-0`,
-        props.shape === 'curved' && props.size === '3xl' && `-end-0 -top-0`,
-        props.shape === 'curved' && props.size === '4xl' && `-end-0 -top-0`,
-        props.dot === 'success' && `bg-success-500`,
-        props.dot === 'primary' && `bg-primary-500`,
-        props.dot === 'info' && `bg-info-500`,
-        props.dot === 'warning' && `bg-warning-500`,
-        props.dot === 'danger' && `bg-danger-500`,
-        props.dot === 'pink' && `bg-pink-500`,
-        props.dot === 'yellow' && `bg-yellow-500`,
-      ]"
+      :class="[dotSizeStyle[props.size], dotPosStyle, dotTypeStyle[props.dot]]"
     ></span>
   </div>
 </template>
