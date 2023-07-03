6d81b81a33b4185dba1ae277962ce523da67b630
Sacha STAFYNIAK <sacha@digisquad.io>
2023-07-03 16:23:30 +0200

    chore(BaseIconBox): fix warning text color

--- components/base/BaseIconBox.vue
+++ components/base/BaseIconBox.vue
@@ -59,7 +59,7 @@ const solidColorVariant = {
 const pastelColorVariant = {
   info: 'bg-info-500/10 text-info-500',
   success: 'bg-success-500/10 text-success-500',
-  warning: 'bg-warning-500/10 text-primary-500',
+  warning: 'bg-warning-500/10 text-warning-500',
   danger: 'bg-danger-500/10 text-danger-500',
   primary: 'bg-primary-500/10 text-primary-500',
   light: 'text-muted-800 bg-white/10',
