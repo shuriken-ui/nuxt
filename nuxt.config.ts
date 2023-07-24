aac7751f23980d5b617be6ffa0f606c3db6f94dc
BIYA Paul <bpsmartdesign@hotmail.com>
2023-07-24 18:03:58 +0100

    feat: review base-accordion design system

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 import { createResolver } from '@nuxt/kit'
 
+// import { withShurikenUI } from './../tailwind/src/'
 import { withShurikenUI } from '@shuriken-ui/tailwind'
 
 const { resolve } = createResolver(import.meta.url)
