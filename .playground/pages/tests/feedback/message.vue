d05a04e1eea987f7abe251cd4ca20cdaab7f55a7
BIYA Paul <bpsmartdesign@hotmail.com>
2023-07-31 09:36:24 +0100

    chore: continue testing component

--- .playground/pages/tests/feedback/message.vue
+++ .playground/pages/tests/feedback/message.vue
@@ -12,41 +12,59 @@ definePageMeta({
     class="flex flex-col gap-12 [&>*]:p-8 [&>:nth-child(odd)]:bg-muted-100 dark:[&>:nth-child(odd)]:bg-muted-900 pb-32"
   >
     <div>
-      <BaseHeading size="xl" weight="medium" class="mb-10">
-        Message Shape
-      </BaseHeading>
+      <div>
+        <BaseHeading size="xl" weight="medium" class="mb-10">
+          Message Shape
+        </BaseHeading>
+      </div>
+      <div class="grid grid-cols-5 gap-2">
+        <BaseMessage
+          type="muted"
+          shape="straight"
+          message="A muted message ..."
+          closable
+        />
+        <BaseMessage
+          type="default"
+          shape="rounded"
+          message="A default message ..."
+          closable
+        />
+        <BaseMessage
+          type="primary"
+          shape="smooth"
+          message="A primary message ..."
+          closable
+        />
+        <BaseMessage
+          type="info"
+          shape="curved"
+          message="An info message ..."
+          closable
+        />
+        <BaseMessage
+          type="success"
+          shape="full"
+          message="A success message ..."
+          closable
+        />
+      </div>
     </div>
-    <div class="grid grid-cols-5 gap-2">
-      <BaseMessage
-        type="muted"
-        shape="straight"
-        message="A muted message ..."
-        closable
-      />
-      <BaseMessage
-        type="default"
-        shape="rounded"
-        message="A default message ..."
-        closable
-      />
-      <BaseMessage
-        type="primary"
-        shape="smooth"
-        message="A primary message ..."
-        closable
-      />
-      <BaseMessage
-        type="default"
-        shape="curved"
-        message="A default message ..."
-        closable
-      />
-      <BaseMessage
-        type="default"
-        shape="full"
-        message="A default message ..."
-        closable
-      />
+    <div>
+      <div>
+        <BaseHeading size="xl" weight="medium" class="mb-10">
+          Message type
+        </BaseHeading>
+      </div>
+      <div class="grid grid-cols-7 gap-2">
+        <BaseMessage type="default" message="A default message ..." closable />
+        <BaseMessage type="muted" message="A muted message ..." closable />
+        <BaseMessage type="primary" message="A primary message ..." closable />
+        <BaseMessage type="info" message="An info message ..." closable />
+        <BaseMessage type="success" message="A success message ..." closable />
+        <BaseMessage type="warning" message="A warning message ..." closable />
+        <BaseMessage type="danger" message="A danger message ..." closable />
+      </div>
     </div>
   </div>
 </template>
