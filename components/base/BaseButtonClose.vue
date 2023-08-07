6496befae81e3e5cd9c80048bb3d8c8c9b5dedce
Biya Paul <77636493+bpsmartdesign@users.noreply.github.com>
2023-08-07 10:55:29 +0100

    feat: match button-close component to related plugin (#53)

--- components/base/BaseButtonClose.vue
+++ components/base/BaseButtonClose.vue
@@ -4,9 +4,17 @@ const props = withDefaults(
     /**
      * The shape of the button.
      */
-    shape?: 'straight' | 'rounded' | 'curved' | 'full'
+    shape?: 'straight' | 'rounded' | 'smooth' | 'curved' | 'full'
     /** The color of the button. Can be 'default' or 'muted. */
-    color?: 'default' | 'muted' | 'primary'
+    color?:
+      | 'default'
+      | 'muted'
+      | 'primary'
+      | 'info'
+      | 'success'
+      | 'warning'
+      | 'danger'
+      | 'none'
   }>(),
   {
     shape: undefined,
@@ -21,26 +29,31 @@ const shape = computed(
 
 const shapeStyle = {
   straight: '',
-  rounded: 'rounded-md',
-  curved: 'rounded-lg',
-  full: 'rounded-full',
+  rounded: 'nui-button-rounded',
+  smooth: 'nui-button-smooth',
+  curved: 'nui-button-curved',
+  full: 'nui-button-full',
 }
+const colorStyle = {
+  default: 'nui-button-default',
+  muted: 'nui-button-muted',
+  primary: 'nui-button-primary',
+  info: 'nui-button-info',
+  success: 'nui-button-success',
+  warning: 'nui-button-warning',
+  danger: 'nui-button-danger',
+  none: '',
+}
+
+const classes = computed(() => [
+  'nui-button-close',
+  shape.value && shapeStyle[shape.value],
+  colorStyle[props.color],
+])
 </script>
 
 <template>
-  <button
-    type="button"
-    class="flex h-9 w-9 items-center justify-center transition-colors duration-300 disabled:opacity-30"
-    :class="[
-      props.color === 'default' &&
-        'hover:bg-muted-100 dark:hover:bg-muted-700 text-muted-700 dark:text-muted-50',
-      props.color === 'muted' &&
-        'bg-muted-100 hover:bg-muted-50 dark:bg-muted-700 dark:hover:bg-muted-600 text-muted-700 dark:text-muted-50',
-      props.color === 'primary' &&
-        'bg-primary-500/10 hover:bg-primary-500/20 text-primary-500',
-      shape && shapeStyle[shape],
-    ]"
-  >
-    <IconClose class="h-4 w-4 fill-current" />
+  <button type="button" :class="classes">
+    <IconClose class="nui-button-icon" />
   </button>
 </template>
