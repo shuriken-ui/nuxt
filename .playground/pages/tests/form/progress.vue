c301ae7d2aa0dca70dd302f1f391280e73d63067
Biya Paul <77636493+bpsmartdesign@users.noreply.github.com>
2023-08-07 11:06:37 +0100

    feat: match progress component to related plugin (#44)

--- .playground/pages/tests/form/progress.vue
+++ .playground/pages/tests/form/progress.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 definePageMeta({
-  title: 'CalendarProgress',
+  title: 'Progress',
   icon: 'tabler:progress-bolt',
-  description: 'SVG icons',
+  description: 'Progress component',
   section: 'form',
 })
 </script>
@@ -13,8 +13,49 @@ definePageMeta({
   >
     <div>
       <BaseHeading size="xl" weight="medium" class="mb-10">
-        CalendarProgress
+        Progress Shape
       </BaseHeading>
+      <div class="grid grid-cols-4 gap-4">
+        <BaseProgress shape="straight" :value="75" />
+        <BaseProgress shape="rounded" :value="75" />
+        <BaseProgress shape="curved" :value="75" />
+        <BaseProgress shape="full" :value="75" />
+      </div>
+    </div>
+    <div>
+      <BaseHeading size="xl" weight="medium" class="mb-10">
+        Progress kind
+      </BaseHeading>
+      <div class="grid grid-cols-5 gap-4">
+        <BaseProgress kind="default" :value="75" />
+        <BaseProgress kind="contrast" :value="75" />
+        <BaseProgress kind="default" />
+        <BaseProgress kind="contrast" />
+      </div>
+    </div>
+    <div>
+      <BaseHeading size="xl" weight="medium" class="mb-10">
+        Progress Colors
+      </BaseHeading>
+      <div class="grid grid-cols-5 gap-4">
+        <BaseProgress color="primary" :value="75" />
+        <BaseProgress color="info" :value="75" />
+        <BaseProgress color="success" :value="75" />
+        <BaseProgress color="warning" :value="75" />
+        <BaseProgress color="danger" :value="75" />
+      </div>
+    </div>
+    <div>
+      <BaseHeading size="xl" weight="medium" class="mb-10">
+        Progress Sizes
+      </BaseHeading>
+      <div class="grid grid-cols-5 gap-4">
+        <BaseProgress size="xs" :value="75" />
+        <BaseProgress size="sm" :value="75" />
+        <BaseProgress size="md" :value="75" />
+        <BaseProgress size="lg" :value="75" />
+        <BaseProgress size="xl" :value="75" />
+      </div>
     </div>
   </div>
 </template>
