91b0919121e9994916781b1c0261e937ce2ba30c
Sacha Stafyniak <sacha.stafyniak@gmail.com>
2023-08-07 16:34:18 +0200

    Revert "feat: match prose component to related plugin (#28)"
    This reverts commit d50a70386e41f9e367eb3825be55caed9416e973.

--- components/base/BaseProse.vue
+++ components/base/BaseProse.vue
@@ -15,15 +15,26 @@ const shape = computed(
   () => props.shape ?? appConfig.nui.defaultShapes?.progress
 )
 
-const shapeStyle = {
-  straight: 'nui-prose-straight',
-  rounded: 'nui-prose-rounded',
-  curved: 'nui-prose-curved',
-}
+const shapeClasses = computed(() => {
+  switch (shape.value) {
+    case 'straight': {
+      return 'prose-img:rounded-none prose-pre:rounded-none prose-table:rounded-none'
+    }
+    case 'rounded': {
+      return 'prose-img:rounded-md prose-pre:rounded-md prose-table:rounded-md prose-table:border-separate prose-table:border-spacing-0 prose-td:border-t prose-td:border-muted-200 dark:prose-td:border-muted-700'
+    }
+    case 'curved': {
+      return 'prose-img:rounded-xl prose-pre:rounded-xl prose-table:rounded-xl prose-table:border-separate prose-table:border-spacing-0 prose-td:border-t prose-td:border-muted-200 dark:prose-td:border-muted-700'
+    }
+  }
+})
 </script>
 
 <template>
-  <div class="nui-prose" :class="shape && shapeStyle[shape]">
+  <div
+    class="prose prose-primary prose-muted dark:prose-invert prose-th:p-4 prose-td:p-4 prose-table:bg-white dark:prose-table:bg-muted-800 prose-table:border prose-table:border-muted-200 dark:prose-table:border-muted-700"
+    :class="[shapeClasses]"
+  >
     <slot></slot>
   </div>
 </template>
