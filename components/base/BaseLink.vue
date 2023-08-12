c57288d23d9305b0cc8f7ec1b80108664d60bbf0
Sacha STAFYNIAK <sacha@digisquad.io>
2023-08-12 02:52:49 +0200

    feat: add BaseLink component

---
+++ components/base/BaseLink.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import type { NuxtLinkProps } from '#app'
+
+const props = defineProps<NuxtLinkProps>()
+const NuxtLink = defineNuxtLink({})
+</script>
+
+<template>
+  <component :is="NuxtLink" class="nui-link" v-bind="props">
+    <slot />
+  </component>
+</template>
