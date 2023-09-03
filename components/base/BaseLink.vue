e49f48fa25c1fd0b7c0e749fb6f00e851fbb6b76
Sacha STAFYNIAK <sacha@digisquad.io>
2023-09-03 18:31:26 +0200

    fix(BaseLink): use internal internal instead of import for props

--- components/base/BaseLink.vue
+++ components/base/BaseLink.vue
@@ -1,6 +1,24 @@
 <script setup lang="ts">
-import type { NuxtLinkProps } from '#app'
+/* eslint-disable vue/require-prop-comment */
+import type { RouteLocationRaw } from '#vue-router'
 
+// We can't use exported types from `#app` because vue doesn't support it yet.
+type NuxtLinkProps = {
+  to?: RouteLocationRaw
+  href?: RouteLocationRaw
+  external?: boolean
+  replace?: boolean
+  custom?: boolean
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  target?: '_blank' | '_parent' | '_self' | '_top' | (string & {}) | null
+  rel?: string | null
+  noRel?: boolean
+  prefetch?: boolean
+  noPrefetch?: boolean
+  activeClass?: string
+  exactActiveClass?: string
+  ariaCurrentValue?: string
+}
 const props = defineProps<NuxtLinkProps>()
 const NuxtLink = defineNuxtLink({})
 </script>
