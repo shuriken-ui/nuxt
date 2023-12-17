e5e492b5bcc9d09452285eb63d9d3b6bbfd0bd75
Driss Chelouati <driss.chelouati@gmail.com>
2023-12-17 02:38:32 +0100

    fix: refactor computed property to handle optional chaining

--- composables/default-property.ts
+++ composables/default-property.ts
@@ -4,5 +4,7 @@ export const useNuiDefaultProperty = (
   property: string,
 ): Ref<string> => {
   const config = (useAppConfig()?.appConfig as any)?.nui
-  return computed(() => properties[property] ?? config[component]?.[property])
+  return computed(
+    () => properties?.[property] ?? config?.[component]?.[property],
+  )
 }
