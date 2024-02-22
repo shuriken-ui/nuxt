fe6f9d320172adcd93754668cdb8827803d46dc9
Driss Chelouati <driss.chelouati@gmail.com>
2024-02-22 14:38:45 +0100

    feat(BaseInput): update class bindings

--- components/form/BaseInput.vue
+++ components/form/BaseInput.vue
@@ -155,10 +155,10 @@ const id = useNinjaId(() => props.id)
 
 const radiuses = {
   none: '',
-  sm: 'nui-input-rounded',
-  md: 'nui-input-smooth',
-  lg: 'nui-input-curved',
-  full: 'nui-input-full',
+  sm: 'nui-input-rounded-sm',
+  md: 'nui-input-rounded-md',
+  lg: 'nui-input-rounded-lg',
+  full: 'nui-input-rounded-full',
 } as Record<string, string>
 
 const sizes = {
